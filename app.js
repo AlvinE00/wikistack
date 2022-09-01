8f051ba86562378940f0730c7e3bde79064092a5
Alvin Espinal <alvinjespinal@gmail.com>
2022-09-01 12:58:32 -0400

    added views and index.js | finished .sync

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 const express = require('express');
 const morgan = require('morgan');
+const { db, Page, User } = require('./models');
+// const html = require('./views/layout');
+
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
-// app.use('/', require('./roots'));
+app.use(express.static(__dirname + '/public'));
+
+db.authenticate().then(() => {
+	console.log('connected to the database');
+});
+
 app.get('/', (req, res) => {
-  res.send('Hello World');
+	res.send('');
 });
 
 const PORT = 1337;
 
-app.listen(PORT, () => {
-  console.log(`app listening in ${PORT}`);
-});
+const init = async () => {
+	await db.sync({ force: true });
+
+	app.listen(PORT, () => {
+		console.log(`app listening in ${PORT}`);
+	});
+};
+
+init();
